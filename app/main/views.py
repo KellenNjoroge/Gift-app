15d969f638fca5b6f5a665ef40bf56c44739104a
KellenNjoroge <muthonkel@gmail.com>
2018-10-24 12:02:18 +0300

    main.retrieve

--- app/main/views.py
+++ app/main/views.py
@@ -3,6 +3,15 @@ from . import main
 from ..models import User
 from flask_login import login_required, current_user
 from .. import db, photos
+import requests
+import os
+import imghdr
+
+key = os.environ.get('API_KEY')
+
+search_url = "https://maps.googleapis.com/maps/api/place/textsearch/json"
+photos_url = "https://maps.googleapis.com/maps/api/place/photo"
+
 
 @main.route('/')
 def index():
@@ -12,3 +21,28 @@ def index():
     title = "Welcome "
 
     return render_template("index.html", title=title)
+
+
+@main.route("/map_retrieve", methods=["GET"])
+def retrieve():
+    return render_template('places.html')
+
+
+@main.route("/sendRequest/<string:query>")
+def results(query):
+    search_payload = {"key": key, "query": query}
+    search_req = requests.get(search_url, params=search_payload)
+    search_json = search_req.json()
+
+    photo_id = search_json["results"][0]["photos"][0]["photo_reference"]
+
+    photo_payload = {"key": key, "maxwidth": 500, "maxwidth": 500, "photoreference": photo_id}
+    photo_request = requests.get(photos_url, params=photo_payload)
+
+    photo_type = imghdr.what("", photo_request.content)
+    photo_name = "static/" + query + "." + photo_type
+
+    with open(photo_name, "wb") as photo:
+        photo.write(photo_request.content)
+
+    return '<img src=' + photo_name + '>'
