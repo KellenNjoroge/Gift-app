f789274bce1c242462f4be9fa003c717b4095680
phoebekaranja <phoebemurish@gmail.com>
2018-10-25 16:11:58 +0300

    all changes

--- app/__init__.py
+++ app/__init__.py
@@ -37,9 +37,9 @@ def create_app(config_name):
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    # Registering the auth blueprint
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint,url_prefix = '/auth')
+    # # Registering the auth blueprint
+    # from .auth import auth as auth_blueprint
+    # app.register_blueprint(auth_blueprint,url_prefix = '/auth')
 
     # configure UploadSet
     configure_uploads(app, photos)
