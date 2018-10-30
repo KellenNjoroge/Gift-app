2aa0dfc42e8dd816d5364b33e4bcece7e3485578
KellenNjoroge <muthonkel@gmail.com>
2018-10-30 17:07:45 +0300

    message

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-export SECRET_KEY=gift
-export MAIL_USERNAME=muthonkel@gmail.com
-export MAIL_PASSWORD=bulgaria36
-export API_KEY=AIzaSyDuQ46CCkH5hYaw-vZyb9TLx-oSkhKE6qg
-python3.6 manage.py server
-
